e9ca1390a76f78d43ccdfc278288ebe2212d7797
4Doigt <djenaneq@gmail.com>
2025-12-15 14:42:30 +0100

    Remodification Gameboard

--- src/Gameboard.cpp
+++ src/Gameboard.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include "Gameboard.hpp"
 
-void draw_game_board_help(std::array<std::array<char,3>,3> & board){
-    for ( std::size_t y {0}; y<3; y++){
-        for (std::size_t x {0}; x<3; x++){
-            board[y][x]='0' + (x+3*y);
-        }
-    }
-}
-
 void draw_game_board(std::array<std::array<char,3>,3> & board){
     for ( std::size_t y {0}; y<3; y++){
         for (std::size_t x {0}; x<3; x++){
